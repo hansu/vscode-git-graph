90b9792dd7e79952373e9603678128aea19bc614
Sam Lin <456807+maxisam@users.noreply.github.com.>
2024-11-26 12:19:31 -0600

    feat: enhance installation script to support custom profile and code name

--- .vscode/install-package.js
+++ .vscode/install-package.js
@@ -1,9 +1,9 @@
 const cp = require('child_process');
 
 const PACKAGED_FILE = './' + process.env.npm_package_name + '-' + process.env.npm_package_version + '.vsix';
-
-console.log('');
-cp.exec('code --install-extension ' + PACKAGED_FILE, { cwd: process.cwd() }, (err, stdout, stderr) => {
+const PROFILE_NAME = process.env.vscode_profile || 'vscode-ext';
+const CODE_NAME = process.env.vscode_name || 'code-insiders';
+cp.exec(`${CODE_NAME} --install-extension ${PACKAGED_FILE} --profile ${PROFILE_NAME}`, { cwd: process.cwd() }, (err, stdout, stderr) => {
 	if (err) {
 		console.log('ERROR:');
 		console.log(err);
